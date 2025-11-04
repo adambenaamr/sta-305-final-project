ec06fadaf285e8c61c6b3104a1d94ec5c0890d85
Adam <yestab335@gmail.com>
2025-11-04 18:19:42 -0500

    Started code for box plot, added a completed bar chart that needs some minor modifications

--- final.R
+++ final.R
@@ -11,7 +11,16 @@ View(coffee)
 
 # Bar chart
 
+# TODO: Change the ordering of the days so that it follows the normal convention
+ggplot(data = coffee, aes(x = Weekday, fill = Time_of_Day)) +
+  geom_bar(position = "dodge") +
+  labs(title = "Bar Distribution of Coffee Sales by Weekday and Time of Day", x = "Weekday", y = "Count", fill = "Time of Day")
+
 # Box plot
 ggplot(data = coffee, aes(x = Weekday, y = Time_of_Day)) +
   geom_boxplot(width = 0.3) +
   theme_minimal()
+
+# Scatter plot
+ggplot(data = coffee, aes(x = Weekday, y = Time_of_Day)) +
+  geom_dotplot()
