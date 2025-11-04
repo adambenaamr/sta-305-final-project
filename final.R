e2779f0b54b716cb088d915f797c96820a9029bc
Adam <yestab335@gmail.com>
2025-11-04 18:26:12 -0500

    Fixed ordering of Weekday appearances and updated bar chart to reflect that change

--- final.R
+++ final.R
@@ -6,12 +6,13 @@ library(ggplot2)
 # Loads the dataset into the variable named coffee
 coffee <- read_csv("final/coffee_sales.csv")
 
+# Reorder the appearances of Weekdays
+coffee$Weekday <- factor(coffee$Weekday, levels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"), ordered = TRUE)
+
 # View the dataset
 View(coffee)
 
 # Bar chart
-
-# TODO: Change the ordering of the days so that it follows the normal convention
 ggplot(data = coffee, aes(x = Weekday, fill = Time_of_Day)) +
   geom_bar(position = "dodge") +
   labs(title = "Bar Distribution of Coffee Sales by Weekday and Time of Day", x = "Weekday", y = "Count", fill = "Time of Day")
